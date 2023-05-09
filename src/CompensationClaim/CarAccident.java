08794e82974a0d409ab81f0eef76cb116f82ac47
suyeon <ibaesuyeon@Naver.com>
2023-05-10 06:15:33 +0900

    [Feat] CompensationClaim, Survey, CarAccident
    CompensationClaim은 독립적인 기능이므로 구현, test 완료.
    Survey, CarAccident는 merge 후 test, 수정 예정.

--- src/CompensationClaim/CarAccident.java
+++ src/CompensationClaim/CarAccident.java
@@ -1,27 +1,76 @@
 package CompensationClaim;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Date;
 
 public class CarAccident extends CompensationClaim {
 
-	private String accidentDetail;
-	private String carNumber;
+	private String type;
 	private Date date;
+	private int time;
+	private String place;
+	private String carNumber;
 	private String driverName;
 	private int licenseNumber;
-	private String place;
-	private int time;
-	private String type;
+	private String accidentDetail;
 
 	public CarAccident() {
 
+	}
+
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getCarNumber() {
+		return carNumber;
+	}
+	public void setCarNumber(String carNumber) {
+		this.carNumber = carNumber;
+	}
+	public String getDriverName() {
+		return driverName;
+	}
+	public void setDriverName(String driverName) {
+		this.driverName = driverName;
+	}
+	public int getLicenseNumber() {
+		return licenseNumber;
+	}
+	public void setLicenseNumber(int licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+	public String getAccidentDetail() {
+		return accidentDetail;
+	}
+	public void setAccidentDetail(String accidentDetail) {
+		this.accidentDetail = accidentDetail;
 	}
 
 	public void finalize() throws Throwable {
 		super.finalize();
 	}
 
-	public String createCompensationClaim() {
-		return "";
-	}
 }// end CarAccident
