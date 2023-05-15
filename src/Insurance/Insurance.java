42344a5cb0dc3311e43dc3630ff657781d44dc5c
BaeJungYeon <qowjdduswkd@naver.com>
2023-05-15 10:13:35 +0900

    [Fix]Insurance equals

--- src/Insurance/Insurance.java
+++ src/Insurance/Insurance.java
@@ -38,7 +38,7 @@ public class Insurance {
 	
     public boolean matchId(String insuranceID) {
 
-return this.insuranceID.equals(insuranceID);
+    return this.insuranceID.equals(insuranceID);
     }
   
     public String toString() {
